0ea4cd28802253e736f398dc37b628d09fc02cf5
FredChen <38094428+greatFredChen@users.noreply.github.com>
2020-06-10 13:43:56 +0800

    Change MPI integral to scatter/gather instead of send/recv.

--- mpi_integral/mpi_integral/mpi_integral.cpp
+++ mpi_integral/mpi_integral/mpi_integral.cpp
@@ -44,45 +44,33 @@ void mpi_integral(int a, int b, int n)
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 	double h = double((b - a)) / n;
 	int local_n = n / Process_Count;
-	double border[2]; // 包括local_a local_b两种
-	double my_sum = 0, total_sum = 0;
+	double *local_a_array = new double[Process_Count];
+	double *local_b_array = new double[Process_Count];
+	double *mysum_array = new double[Process_Count];
+	double local_a, local_b, my_sum;
 	MPI_Status status;
 	// 计时
 	start_time();
-	// 分支线程只负责运算
-	if (myid != 0)
+	// scatter分发数据
+	for (int i = 0; i < Process_Count; i++)
 	{
-		MPI_Recv(&border[0], 2, MPI_DOUBLE, 0, myid, MPI_COMM_WORLD, &status);
-		double local_a = border[0];
-		double local_b = border[1];
-		local_n = round((local_b - local_a) / h);
-		my_sum = (f(local_a) + f(local_b)) / 2.0;
-		for (int i = 1; i < local_n; i++)
-			my_sum += f(local_a + i * h);
-		// 发送数据给主线程
-		MPI_Send(&my_sum, 1, MPI_DOUBLE, 0, myid, MPI_COMM_WORLD);
+		local_a_array[i] = a + i * local_n * h;
+		local_b_array[i] = (i == Process_Count - 1) ? b : local_a_array[i] + local_n * h;
 	}
-	else // 主线程负责运算和分配任务
+	MPI_Scatter(local_a_array, 1, MPI_DOUBLE, &local_a, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	MPI_Scatter(local_b_array, 1, MPI_DOUBLE, &local_b, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	// 分支线程只负责运算
+	local_n = round((local_b - local_a) / h);
+	my_sum = (f(local_a) + f(local_b)) / 2.0;
+	for (int i = 1; i < local_n; i++)
+		my_sum += f(local_a + i * h);
+	// 使用MPI_Gatter收集数据
+	MPI_Gather(&my_sum, 1, MPI_DOUBLE, mysum_array, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	if (myid == 0)
 	{
-		for (int i = 1; i < Process_Count; i++)
-		{
-			border[0] = a + i * local_n * h;
-			border[1] = (i == Process_Count - 1) ? b : border[0] + local_n * h;
-			MPI_Send(&border[0], 2, MPI_DOUBLE, i, i, MPI_COMM_WORLD);
-		}
-		for (int i = 1; i < Process_Count; i++)
-		{
-			MPI_Recv(&my_sum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			total_sum += my_sum;
-		}
-
-		// 主进程运算自己的那一部分
-		double local_a = a;
-		double local_b = local_a + local_n * h;
-		my_sum = (f(local_a) + f(local_b)) / 2.0;
-		for (int i = 1; i < local_n; i++)
-			my_sum += f(local_a + i * h);
-		total_sum += my_sum;
+		double total_sum = 0.0;
+		for (int i = 0; i < Process_Count; i++)
+			total_sum += mysum_array[i];
 		end_time(way::mpi);
 		printf("(mpi)The result of integral is: %f\n", total_sum * h);
 	}
